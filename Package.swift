b15567b5cde8f25cdd5b56a602a68c5367e3fd98
Daniel Leping <dileping@gmail.com>
2017-03-25 21:17:46 +0200

    cleanup

--- Package.swift
+++ Package.swift
@@ -6,26 +6,9 @@ let package = Package(
         Target(
             name: "RDBC"
         ),
-        Target(
-            name: "RDBCSQLite",
-            dependencies: [.Target(name: "RDBC")]
-        ),
-        Target(
-            name: "Swirl",
-            dependencies: [.Target(name: "RDBC")]
-        ),
-        Target(
-            name: "SwirlSQLite",
-            dependencies: [.Target(name: "Swirl"), .Target(name: "RDBCSQLite")]
-        ),
-        Target(
-            name: "Demo",
-            dependencies: [.Target(name: "Swirl"), .Target(name: "SwirlSQLite")]
-        )
     ],
     dependencies: [
         .Package(url: "https://github.com/reactive-swift/Future.git", "0.2.0-alpha"),
         //.Package(url: "https://github.com/IBM-Swift/CLibpq.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://github.com/carlbrown/CSQLite.git", majorVersion: 0, minor: 0),
     ]
 )
