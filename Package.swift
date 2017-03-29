1e23b83050d55fa38f66b7f71a12e0c6ee916cea
Daniel Leping <dileping@gmail.com>
2017-03-29 17:57:12 +0300

    Swift 3.1 support

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .Package(url: "https://github.com/reactive-swift/Future.git", "0.2.0-alpha"),
+        .Package(url: "https://github.com/reactive-swift/Future.git", majorVersion: 0, minor: 2),
         //.Package(url: "https://github.com/IBM-Swift/CLibpq.git", majorVersion: 0, minor: 1),
     ]
 )
